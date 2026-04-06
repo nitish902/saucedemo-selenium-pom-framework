d70a64b0b645d0d2e7c545fa01e92ba7e53573ce
Nitish Shukla <nitishshukla902@gmail.com>
2026-04-06 16:45:25 +0530

    Add open method to navigate to login page
    Added a method to open the login page URL.

--- saucedemo_pom/saucedemo_pom/pages/login_page.py
+++ saucedemo_pom/saucedemo_pom/pages/login_page.py
@@ -4,11 +4,18 @@ from pages.base_page import BasePage
 
 class LoginPage(BasePage):
 
+    URL = "https://www.saucedemo.com/"
+
     USERNAME = (By.ID, "user-name")
 
     PASSWORD = (By.ID, "password")
 
     LOGIN_BTN = (By.ID, "login-button")
+
+
+    def open(self):
+
+        self.driver.get(self.URL)
 
 
     def login(self, username, password):
