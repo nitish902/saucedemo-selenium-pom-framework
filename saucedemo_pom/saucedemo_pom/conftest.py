2e4efe9e16476eb4825ce188b4668eca9b9058d5
Nitish Shukla <nitishshukla902@gmail.com>
2026-04-06 15:39:26 +0530

    Enhance driver fixture with login and new options
    Updated the driver fixture to include login functionality and modified Chrome options.

--- saucedemo_pom/saucedemo_pom/conftest.py
+++ saucedemo_pom/saucedemo_pom/conftest.py
@@ -4,22 +4,36 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+from pages.login_page import LoginPage
 
-@pytest.fixture
+
+@pytest.fixture(scope="function")
 def driver():
 
     chrome_options = Options()
 
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(
+
         service=Service(ChromeDriverManager().install()),
         options=chrome_options
+
     )
 
-    driver.maximize_window()
+    driver.implicitly_wait(10)
+
+    # open saucedemo
+    driver.get("https://www.saucedemo.com")
+
+    # login before every test
+    login_page = LoginPage(driver)
+
+    login_page.login("standard_user", "secret_sauce")
 
     yield driver
 
