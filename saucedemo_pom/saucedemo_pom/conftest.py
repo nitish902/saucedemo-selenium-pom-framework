98944bd7dd7f137fd0d0e795fa915dea4b1ca37a
Nitish Shukla <nitishshukla902@gmail.com>
2026-04-06 16:35:54 +0530

    Update conftest.py

--- saucedemo_pom/saucedemo_pom/conftest.py
+++ saucedemo_pom/saucedemo_pom/conftest.py
@@ -4,8 +4,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-from pages.login_page import LoginPage
-
 
 @pytest.fixture(scope="function")
 def driver():
@@ -13,25 +11,29 @@ def driver():
     chrome_options = Options()
 
     chrome_options.add_argument("--headless=new")
+
     chrome_options.add_argument("--window-size=1920,1080")
+
     chrome_options.add_argument("--disable-gpu")
+
     chrome_options.add_argument("--no-sandbox")
+
     chrome_options.add_argument("--disable-dev-shm-usage")
 
+    chrome_options.add_argument("--disable-infobars")
+
+    chrome_options.add_argument("--disable-extensions")
+
+
     driver = webdriver.Chrome(
+
         service=Service(ChromeDriverManager().install()),
+
         options=chrome_options
+
     )
 
     driver.implicitly_wait(10)
-
-    # open login page
-    driver.get("https://www.saucedemo.com")
-
-    # perform login using your existing method
-    login_page = LoginPage(driver)
-
-    login_page.login("standard_user", "secret_sauce")
 
     yield driver
 
